1989b3dc5d1189f073d97133e40a5020ae4021e8
Jaime <jaxmex5885@gmail.com>
2026-06-01 18:17:16 +0200

    Funcion division

--- practicaGit.py
+++ practicaGit.py
@@ -5,3 +5,10 @@ print(suma(2,4))
 def multiplicacion(x,y):
     return x*y
 print(multiplicacion(2,4))
+
+def division(x,y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        print("No se puede dividir un número entre 0")
+print(division(4,0))
